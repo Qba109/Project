d92e987f6b6e54e1548a5bbd93087419e549719b
Kuba Lipiec <kuba.lipiec@gmail.com>
2022-10-21 17:48:26 +0200

    Extract scanner to separate variable

--- project/src/main/java/org/example/Main.java
+++ project/src/main/java/org/example/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Wpisz imie: ");
-        String line = new Scanner(System.in).next();
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.next();
 
         System.out.println("Mam na imie: " + line);
     }
