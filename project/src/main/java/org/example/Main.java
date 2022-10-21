d10c5cefefa1ac808fd38ba5341297554cc6d524
Kuba Lipiec <kuba.lipiec@gmail.com>
2022-10-21 17:43:34 +0200

    Fixed idea bug

--- project/src/main/java/org/example/Main.java
+++ project/src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String Custom = System.console().readLine();
-        System.out.println(Custom);
+        System.out.print("Wpisz imie: ");
+        String line = new Scanner(System.in).next();
+
+        System.out.println("Mam na imie: " + line);
     }
 }
