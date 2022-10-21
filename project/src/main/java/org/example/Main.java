2136459f6783159331978a327007d7a005f67f98
Kuba Lipiec <kuba.lipiec@gmail.com>
2022-10-21 17:51:03 +0200

    Add task 1

--- project/src/main/java/org/example/Main.java
+++ project/src/main/java/org/example/Main.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
+
+    private static void task1() {
+        // Task 1
+        // * Take to integers (a, b)
+        // * divide them by each other
+        // * return as double
+        //
+        // i.e.
+        //  input: 1, 2
+        //  output: 0.5
+        //
+
         System.out.print("Wpisz imie: ");
         Scanner scanner = new Scanner(System.in);
         String line = scanner.next();
